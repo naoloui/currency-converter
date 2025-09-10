341b24a0313522c7717e02825d2a97fa9dd30dd9
naoloui <naomi.laufer16@gmail.com>
2025-09-10 17:29:44 +0200

    Social media icons added to the footer of index.html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ConverterForm from "./ConverterForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="CurrencyConverter">
+      <h2 className="converter-title">Currency Converter</h2>
+      <ConverterForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Output.js
@@ -0,0 +1,3 @@
+<div class="output">
+  <h2>Converted values in different currencies</h2>
+</div>;
